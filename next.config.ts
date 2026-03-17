ff59c927422b7d2c05d1fcb5f65e8b9676431adb
likhithgowdaslikhiths-cmd <likhithgowdaslikhiths@gmail.com>
2026-03-17 09:34:32 +0530

    fix nextjs cache issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
